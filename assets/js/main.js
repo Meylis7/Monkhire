008ef04f20fefd8504f92da1d15c3c872f8b13d6
Meylis Gazakow <Meylis7@users.noreply.github.com>
2022-05-08 16:49:31 +0300

    new update with animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,8 +86,63 @@ if (document.getElementById("console") != undefined) {
             }
         }, 400)
     }
-
 }
+
+
+if (document.getElementById("console2") != undefined) {
+    consoleText2(['ex-Instagram', 'ex-Facebook', 'ex-Twitter','ex-YouTube'], 'text2', ['#F85052', '#F85052', '#F85052']);
+
+function consoleText2(words, id, colors) {
+    if (colors === undefined) colors = ['#fff'];
+    var visible = true;
+    var con = document.getElementById('console2');
+    var letterCount = 1;
+    var x = 1;
+    var waiting = false;
+    var target = document.getElementById(id)
+    target.setAttribute('style', 'color:' + colors[0])
+    window.setInterval(function () {
+
+        if (letterCount === 0 && waiting === false) {
+            waiting = true;
+            target.innerHTML = words[0].substring(0, letterCount)
+            window.setTimeout(function () {
+                var usedColor = colors.shift();
+                colors.push(usedColor);
+                var usedWord = words.shift();
+                words.push(usedWord);
+                x = 1;
+                target.setAttribute('style', 'color:' + colors[0])
+                letterCount += x;
+                waiting = false;
+            }, 1000)
+        } else if (letterCount === words[0].length + 1 && waiting === false) {
+            waiting = true;
+            window.setTimeout(function () {
+                x = -1;
+                letterCount += x;
+                waiting = false;
+            }, 1000)
+        } else if (waiting === false) {
+            target.innerHTML = words[0].substring(0, letterCount)
+            letterCount += x;
+        }
+    }, 120)
+    window.setInterval(function () {
+        if (visible === true) {
+            con.className = 'console-underscore2 hidden';
+            visible = false;
+
+        } else {
+            con.className = 'console-underscore2';
+
+            visible = true;
+        }
+    }, 400)
+}
+}
+
+
 // Switch Text end =============================
 
 
